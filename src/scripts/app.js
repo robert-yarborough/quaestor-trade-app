53c1a440bccef2465a8ae98a4db8437d2776fa3d
Igor Glushko <glushko@bluefountainmedia.com>
2018-10-30 14:42:11 +0200

    Start implement style for platform page

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -95,6 +95,42 @@ const finance_slider_destroy = () => {
 	}
 };
 
+let $feature_slider = $('.js-features-block-slider');
+let $feature_slider_wrap = $feature_slider.find('.features-block__grid');
+let $feature_slider_item = $feature_slider.find('.features-block__col');
+let $nav_prev = $('<div class="swiper-button-prev js-ft-sl-btn-prev"/>');
+let $nav_next = $('<div class="swiper-button-next js-ft-sl-btn-next"/>');
+let featureSwiper;
+
+if ($feature_slider.length) {
+    $feature_slider_wrap.addClass('swiper-wrapper');
+    $feature_slider_item.addClass('swiper-slide');
+    $feature_slider_wrap.wrap('<div class="swiper-container" />');
+    $nav_prev.insertAfter($feature_slider_wrap);
+    $nav_next.insertAfter($feature_slider_wrap);
+}
+
+const feature_slider_init = () => {
+    if ($feature_slider.length && !$feature_slider.hasClass(init_slider_class)) {
+        let $feature_slider_box = $feature_slider.find('.swiper-container');
+        featureSwiper = new Swiper ($feature_slider_box, {
+            speed: 750,
+            navigation: {
+                nextEl: '.js-ft-sl-btn-next',
+                prevEl: '.js-ft-sl-btn-prev',
+            },
+        });
+        $feature_slider.addClass(init_slider_class);
+    }
+};
+
+const feature_slider_destroy = () => {
+    if ($feature_slider.length && $feature_slider.hasClass(init_slider_class)) {
+        featureSwiper.destroy(false, true);
+        $feature_slider.removeClass(init_slider_class);
+    }
+};
+
 const animate_init = () => {
     skrollr.init();
 };
@@ -106,9 +142,11 @@ const animate_destroy = () => {
 addMediaQueryListener(devicesMQ.mobileMQ, function (match) {
 	if (match) {
 		finance_slider_init();
+        feature_slider_init();
 	}
 	else if (match == false) {
 		finance_slider_destroy();
+        feature_slider_destroy();
 	}
 });
 
