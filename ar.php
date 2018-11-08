c1357337ff78f9e943a30ec6586ea1a3675a59b6
Igor Glushko <glushko@bluefountainmedia.com>
2018-11-08 13:55:57 +0200

    Fixed scripts

--- ar.php
+++ ar.php
@@ -237,7 +237,7 @@
             </div>
         </div>
     </div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d730.8960676067617!2d47.976355218695005!3d29.377689921102657!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3fcf84ee7ad6bbed%3A0xe39f13b634b3c0c1!2sBoursa+Kuwait!5e0!3m2!1suk!2sua!4v1541672219163" width="800" height="600" frameborder="0" style="border:0" allowfullscreen>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d730.8960676067617!2d47.976355218695005!3d29.377689921102657!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3fcf84ee7ad6bbed%3A0xe39f13b634b3c0c1!2sBoursa+Kuwait!5e0!3m2!1suk!2sua!4v1541672219163" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
 </div>
 <footer class="main-footer" role="contentinfo">
     <div class="page-frame">
