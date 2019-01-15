52d51cfd2a8bc6420720d5101a3d607b7a8c3451
Alex Lyalyuk <alex.lyalyuk@pactera.com>
2019-01-15 17:08:50 +0200

    create multipurpose paragraph for 2 column images

--- scripts/qa/phpcs_local.sh
+++ scripts/qa/phpcs_local.sh
@@ -9,10 +9,10 @@ composer global require dealerdirect/phpcodesniffer-composer-installer
 
 $(composer config --global home)/vendor/bin/phpcbf  --standard=Drupal --extensions=php,module,inc,install,test,theme,info  ${PROJECT_ROOT}/modules/custom
 $(composer config --global home)/vendor/bin/phpcbf  --standard=DrupalPractice --extensions=php,module,inc,install,test,theme ${PROJECT_ROOT}/modules/custom
-$(composer config --global home)/vendor/bin/phpcbf  --standard=Drupal --extensions=php,module,inc,install,test,theme,info --ignore=*.css,*.md ${PROJECT_ROOT}/themes/custom
-$(composer config --global home)/vendor/bin/phpcbf  --standard=DrupalPractice --extensions=php,module,inc,install,test,theme --ignore=*.css,*.md ${PROJECT_ROOT}/themes/custom
+$(composer config --global home)/vendor/bin/phpcbf  --standard=Drupal --extensions=php,module,inc,install,test,theme,info --ignore=*.css,*.md,*/node_modules/* ${PROJECT_ROOT}/themes/custom
+$(composer config --global home)/vendor/bin/phpcbf  --standard=DrupalPractice --extensions=php,module,inc,install,test,theme --ignore=*.css,*.md,*/node_modules/* ${PROJECT_ROOT}/themes/custom
 
 $(composer config --global home)/vendor/bin/phpcs  --standard=Drupal --extensions=php,module,inc,install,test,theme,info ${PROJECT_ROOT}/modules/custom
 $(composer config --global home)/vendor/bin/phpcs  --standard=DrupalPractice --extensions=php,module,inc,install,test,theme ${PROJECT_ROOT}/modules/custom
-$(composer config --global home)/vendor/bin/phpcs  --standard=Drupal --extensions=php,module,inc,install,test,theme,info --ignore=*.css,*.md  ${PROJECT_ROOT}/themes/custom
-$(composer config --global home)/vendor/bin/phpcs  --standard=DrupalPractice --extensions=php,module,inc,install,test,theme --ignore=*.css,*.md ${PROJECT_ROOT}/themes/custom
+$(composer config --global home)/vendor/bin/phpcs  --standard=Drupal --extensions=php,module,inc,install,test,theme,info --ignore=*.css,*.md,*/node_modules/*  ${PROJECT_ROOT}/themes/custom
+$(composer config --global home)/vendor/bin/phpcs  --standard=DrupalPractice --extensions=php,module,inc,install,test,theme --ignore=*.css,*.md,*/node_modules/* ${PROJECT_ROOT}/themes/custom
