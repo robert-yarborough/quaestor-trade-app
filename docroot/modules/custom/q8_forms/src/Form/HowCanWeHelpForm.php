4d6a34f1d53e5629bb023324cf417c2f7aa03aa3
Alex Lyalyuk <alex.lyalyuk@pactera.com>
2019-01-21 17:49:34 +0200

    init forms

---
+++ docroot/modules/custom/q8_forms/src/Form/HowCanWeHelpForm.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Drupal\q8_forms\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+
+/**
+ * Class HowCanWeHelpForm.
+ */
+class HowCanWeHelpForm extends FormBase {
+
+  /**
+   * Current request.
+   *
+   * @var \Symfony\Component\HttpFoundation\Request
+   */
+  protected $request;
+
+  /**
+   * Constructs a new HowCanWeHelpForm object.
+   */
+  public function __construct(Request $request) {
+    $this->requestStack = $request;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('request_stack')->getCurrentRequest()
+    );
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'how_can_we_help_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $title = $this->t('How can we help?');
+
+    $form['title'] = [
+      '#markup' => '<h1>' . $title . '</h1>',
+    ];
+
+    $form['how_can_we_help'] = [
+      '#type' => 'textfield',
+      '#attributes' => [
+        'placeholder' => $this->t('Ask us anything. We are experts!')
+      ],
+      '#title' => $title,
+      '#title_display' => 'invisible',
+    ];
+
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+  }
+
+}
---
+++ docroot/modules/custom/q8_forms/src/Plugin/Block/HowCanWeHelpBlock.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\q8_forms\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Form\FormBuilderInterface;
+
+/**
+ * Provides a 'HowCanWeHelpBlock' block.
+ *
+ * @Block(
+ *  id = "how_can_we_help_block",
+ *  admin_label = @Translation("How can we help block"),
+ * )
+ */
+class HowCanWeHelpBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Drupal\Core\Form\FormBuilderInterface definition.
+   *
+   * @var \Drupal\Core\Form\FormBuilderInterface
+   */
+  protected $formBuilder;
+  /**
+   * Constructs a new HowCanWeHelpBlock object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param string $plugin_definition
+   *   The plugin implementation definition.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    FormBuilderInterface $form_builder
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $form_builder;
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder')
+    );
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return $this->formBuilder->getForm('how_can_we_help_form');
+  }
+
+}
