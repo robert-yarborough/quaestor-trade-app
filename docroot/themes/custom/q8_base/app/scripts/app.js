ed74a4d692c5e906eb4bb085a03971cf71ed486f
Egor Solonchenko <egor.solonchenko@pactera.com>
2019-03-05 16:32:18 +0200

    q8_base theme

---
+++ docroot/themes/custom/q8_base/app/scripts/app.js
@@ -0,0 +1,7 @@
+import {greet} from './hello';
+import {names} from 'Config'; // Example working alias from webpack.config.js
+
+greet(names.user1);
+
+console.log($) // Jquery installed for global usage in webpack.config.js
+
