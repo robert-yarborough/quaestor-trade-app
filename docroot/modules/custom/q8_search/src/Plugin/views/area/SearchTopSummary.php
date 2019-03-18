e3df98f444fb5f773550df8f26d6ca9e8d4a69d1
Egor Solonchenko <egor.solonchenko@pactera.com>
2019-03-18 17:46:59 +0200

    change label for search

--- docroot/modules/custom/q8_search/src/Plugin/views/area/SearchTopSummary.php
+++ docroot/modules/custom/q8_search/src/Plugin/views/area/SearchTopSummary.php
@@ -22,17 +22,31 @@ class SearchTopSummary extends AreaPluginBase implements ContainerFactoryPluginI
       return [];
     }
 
+    $search_query = $this->view->getRequest()->get('how_can_we_help');
+    $number_results = isset($this->view->total_rows) ? $this->view->total_rows : count($this->view->result);
+    if (!empty($search_query)) {
+      $search_label = $this->t(
+        '@total results for <em>"@query"</em>',
+        [
+          '@total' => $number_results,
+          '@query' => $search_query,
+        ]
+      );
+    }
+    else{
+      $search_label = $this->t(
+        '@total results',
+        [
+          '@total' => $number_results,
+        ]
+      );
+    }
+
     return [
       '#cache' => [
         'contexts' => ['url.query_args:how_can_we_help'],
       ],
-      '#markup' => $this->t(
-        '@total results for <em>"@query"</em>',
-        [
-          '@total' => isset($this->view->total_rows) ? $this->view->total_rows : count($this->view->result),
-          '@query' => $this->view->getRequest()->get('how_can_we_help'),
-        ]
-      ),
+      '#markup' => $search_label,
     ];
 
   }
